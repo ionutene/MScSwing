1e4d2444af4c5f379521d7ec34f900daf46cb071
Hesus <postelnicu.george@gmail.com>
2017-03-12 19:00:33 +0200

    Removed commented part.

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -5,8 +5,8 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         try {
             Map<String, Set<String>> formulas = CSVParser.readCSV();
 //            System.out.println(formulas);
@@ -17,38 +17,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        /*System.out.println("__________________________________1__________________________________");
-
-        for (String[] str : matrix) {
-            for (String elem : str) {
-                System.out.print(elem + "\t");
-            }
-            System.out.println("");
-        }
-        int size = 0;
-        System.out.println("__________________________________2__________________________________");
-        for (String b : routes.keySet()) {
-            //Arrays.toString(Object[] a)
-            System.out.println("K(" + b + ")= " + Arrays.asList(routes.get(b)));
-            size++;
-        }
-
-        System.out.println("__________________________________3__________________________________");
-        int i = 0;
-        //System.out.println(sb);
-        String[] set = new String[size];
-        for (String b : routes.keySet()) {
-            set[i] = b;
-            i++;
-        }
-        Subset m = new Subset();
-        m.printSubsets(set);
-
-        System.out.println("__________________________________Final______________________________");*/
-
-
     }
-
 
 }
